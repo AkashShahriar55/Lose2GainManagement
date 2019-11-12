b276f8d58582b05b4b6e7407231c7603bef5914d
Bishal Imtiaz <bishaimtiaz60@gmail.com>
2019-11-13 02:10:43 +0600

    Form Design : activity for adding client image is added

--- app/src/main/java/com/example/lose2gainmanagement/AddClient.java
+++ app/src/main/java/com/example/lose2gainmanagement/AddClient.java
@@ -6,7 +6,10 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.lose2gainmanagement.ui.form.ClientImage;
 import com.example.lose2gainmanagement.ui.form.FormPagerAdapter;
 import com.example.lose2gainmanagement.ui.form.MeasuremntFragment;
 import com.example.lose2gainmanagement.ui.form.MedicalProblemFragment;
@@ -18,21 +21,75 @@ import java.util.List;
 
 public class AddClient extends AppCompatActivity {
 
+   private Button next_btn;
+    private Button previous_btn;
+    private int position;
+    private FormPagerAdapter adapter;
+    private ViewPager formPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_client);
 
-        ViewPager formPager = findViewById(R.id.formPager);
+        formPager = findViewById(R.id.formPager);
         TabLayout form_indicator = findViewById(R.id.form_indicator);
+
+        next_btn = findViewById(R.id.next_btn);
+        previous_btn = findViewById(R.id.previous_btn);
 
         List<Fragment> list = new ArrayList<>();
         list.add(new PersonalInfoFragment());
         list.add(new MeasuremntFragment());
         list.add(new MedicalProblemFragment());
+        list.add(new ClientImage(AddClient.this));
 
-        FormPagerAdapter adapter = new FormPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, AddClient.this, list);
+         adapter = new FormPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, AddClient.this, list);
         formPager.setAdapter(adapter);
         form_indicator.setupWithViewPager(formPager);
+
+        next_btn.setOnClickListener(view -> formPager.setCurrentItem(formPager.getCurrentItem() + 1,true));
+
+        previous_btn.setOnClickListener(view -> formPager.setCurrentItem(formPager.getCurrentItem() - 1,true));
+
+        pageChange();
     }
+
+
+    private void pageChange() {
+
+        formPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+
+                if(position == 0){
+                    previous_btn.setVisibility(View.INVISIBLE);
+                    next_btn.setVisibility(View.VISIBLE);
+                }
+                else if (position == adapter.getCount() - 1){
+                    next_btn.setVisibility(View.INVISIBLE);
+                    previous_btn.setVisibility(View.VISIBLE);
+                }
+                else{
+                    next_btn.setVisibility(View.VISIBLE);
+                    previous_btn.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
 }
---
+++ app/src/main/java/com/example/lose2gainmanagement/ui/form/ClientImage.java
@@ -0,0 +1,191 @@
+package com.example.lose2gainmanagement.ui.form;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.lose2gainmanagement.MainActivity;
+import com.example.lose2gainmanagement.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.IOException;
+import java.util.List;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import static com.yalantis.ucrop.UCropFragment.TAG;
+
+public class ClientImage extends Fragment {
+
+
+    private Context context;
+    private static final int REQUEST_IMAGE = 100;
+    private ImageView imageView;
+
+    public ClientImage() {
+    }
+
+    public ClientImage(Context context) {
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.form_client_pic,container,false);
+
+        CardView client_pic = rootView.findViewById(R.id.client_pic);
+        imageView = rootView.findViewById(R.id.client_image);
+        CardView edit_pic = rootView.findViewById(R.id.edit_pic);
+
+        client_pic.setOnClickListener(view -> selectImage());
+
+        edit_pic.setOnClickListener(view -> selectImage());
+
+        return rootView;
+    }
+
+
+
+    private void showImagePickerOptions() {
+        ImagePickerActivity.showImagePickerOptions(context, new ImagePickerActivity.PickerOptionListener() {
+            @Override
+            public void onTakeCameraSelected() {
+                launchCameraIntent();
+            }
+
+            @Override
+            public void onChooseGallerySelected() {
+                launchGalleryIntent();
+            }
+        });
+    }
+
+    private void launchCameraIntent() {
+        Intent intent = new Intent(context, ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_IMAGE_CAPTURE);
+
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+
+        // setting maximum bitmap width and height
+        intent.putExtra(ImagePickerActivity.INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT, true);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_WIDTH, 1000);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_HEIGHT, 1000);
+
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    private void launchGalleryIntent() {
+        Intent intent = new Intent(context, ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_GALLERY_IMAGE);
+
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = data.getParcelableExtra("path");
+                try {
+                    // You can update this bitmap to your server
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
+
+                    // loading profile image from local cache
+                    loadProfile(uri.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Showing Alert Dialog with Settings option
+     * Navigates user to app settings
+     * NOTE: Keep proper title and message depending on your app
+     */
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(getString(R.string.dialog_permission_title));
+        builder.setMessage(getString(R.string.dialog_permission_message));
+        builder.setPositiveButton(getString(R.string.go_to_settings), (dialog, which) -> {
+            dialog.cancel();
+            openSettings();
+        });
+        builder.setNegativeButton(getString(android.R.string.cancel), (dialog, which) -> dialog.cancel());
+        builder.show();
+
+    }
+
+    // navigating user to app settings
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
+    }
+
+
+    private void loadProfile(String url) {
+        Log.d(TAG, "Image cache path: " + url);
+
+        GlideApp.with(this).load(url)
+                .into(imageView);
+        //imageView.setColorFilter(ContextCompat.getColor(context, android.R.color.transparent));
+    }
+
+    private void selectImage() {
+        Dexter.withActivity((Activity) context)
+                .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            showImagePickerOptions();
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+
+
+}
